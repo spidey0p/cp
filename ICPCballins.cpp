5f9dd3b34f7e0269461ea6365bd58d906ebccb96
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-02-15 07:51:15 +0530

    Add files via upload

---
+++ ICPCballins.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] >= 1 && arr[i] <= 7)
+                count = i;
+        }
+        cout << count + 1 << endl;
+    }
+    return 0;
+}
